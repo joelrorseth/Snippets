d107e62b0b0d0460fd43f146aa3388f4af5915df
Joel Rorseth <joelrorseth@gmail.com>
2018-07-04 22:11:25 -0400

    Implement weighted undir acycl graph in Python

---
+++ Python Snippets/Data Structures/wu_graph.py
@@ -0,0 +1,41 @@
+#
+# Weighted Undirected Acyclical Graph
+# Joel Rorseth
+#
+
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self):
+        self.adj = defaultdict(set)
+        self.vert = set()
+
+    def connect(self, v1, v2, weight):
+        self.adj[v1].add((v2, weight))
+        self.adj[v2].add((v1, weight))
+        self.vert.add(v1)
+        self.vert.add(v2)
+
+    def dfs(self, start):
+
+        v = set()
+        stack = [start]
+
+        while stack:
+            cur = stack.pop()
+
+            if cur not in v:
+                v.add(cur)
+
+                print(cur)
+                stack += [ v for v,w in self.adj[cur] ]
+
+
+g = Graph()
+g.connect(1,2,3)
+g.connect(2,3,4)
+g.connect(2,6,2)
+g.connect(4,6,6)
+g.connect(5,6,5)
+g.dfs(1)
