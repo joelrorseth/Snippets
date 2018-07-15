74b80291c4d0a6bdd79f137bd91251e03849dc10
Joel Rorseth <joelrorseth@gmail.com>
2018-07-15 12:13:09 -0400

    Add DAG implementation in Python

--- Python Snippets/Data Structures/dag.py
+++ Python Snippets/Data Structures/dag.py
@@ -1,9 +1,9 @@
 #
-# Weighted Undirected Acyclical Graph
+# Weighted Directed Acyclical Graph (DAG)
 # Joel Rorseth
 #
 
-from collections import defaultdict
+from collections import defaultdict, deque
 
 
 class Graph:
@@ -13,29 +13,41 @@ class Graph:
 
     def connect(self, v1, v2, weight):
         self.adj[v1].add((v2, weight))
-        self.adj[v2].add((v1, weight))
         self.vert.add(v1)
         self.vert.add(v2)
 
     def dfs(self, start):
-
-        v = set()
+        vis = set()
         stack = [start]
 
         while stack:
             cur = stack.pop()
 
-            if cur not in v:
-                v.add(cur)
+            if cur not in vis:
+                vis.add(cur)
 
                 print(cur)
                 stack += [ v for v,w in self.adj[cur] ]
+
+    def bfs(self, start):
+        vis = set()
+        q = deque([start])
+
+        while q:
+            cur = q.popleft()
+
+            if cur not in vis:
+                vis.add(cur)
+
+                print(cur)
+                for v,w in self.adj[cur]:
+                    q.append(v)
 
 
 g = Graph()
 g.connect(1,2,3)
 g.connect(2,3,4)
 g.connect(2,6,2)
-g.connect(4,6,6)
-g.connect(5,6,5)
-g.dfs(1)
+g.connect(3,5,6)
+g.connect(5,7,5)
+g.bfs(1)
