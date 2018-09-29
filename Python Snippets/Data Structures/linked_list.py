42976e60a3f722003b07ec90372ebae962f2a064
Joel Rorseth <joelrorseth@gmail.com>
2018-09-29 11:08:51 -0400

    Add remove function to linked list

--- Python Snippets/Data Structures/linked_list.py
+++ Python Snippets/Data Structures/linked_list.py
@@ -17,12 +17,29 @@ class LinkedList:
         n.next = self.head
         self.head = n
 
+    def remove(self,val):
+        slow = None
+        fast = self.head
+
+        while fast:
+            if fast.val == val:
+                if slow:
+                    slow.next = fast.next
+                else:
+                    self.head = self.head.next
+                return True
+
+            slow = fast
+            fast = fast.next
+
+        return False
+
     def print(self):
         n = self.head
         while n:
-            print(n.val)
+            print(n.val, end=" -> ")
             n = n.next
-
+        print("None")
 
 
 ll = LinkedList()
@@ -31,4 +48,8 @@ ll.insert(2)
 ll.insert(3)
 ll.insert(4)
 ll.insert(5)
+ll.print()
+ll.remove(3)
+ll.print()
+ll.remove(5)
 ll.print()
